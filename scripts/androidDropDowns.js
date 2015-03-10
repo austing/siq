570a04c65376782061e83dacaca44c9019de689e
Austin Gross <austing@gmail.com>
2015-03-10 18:11:05 +0100

    improved js

--- scripts/androidDropDowns.js
+++ scripts/androidDropDowns.js
@@ -1,11 +1,16 @@
 jQuery(document).ready(function(){
-    if(Modernizr.touch){
-        jQuery('#topnav').on('click', 'li.menu-item > a', function(e){
-            if(!jQuery(this).data('open')){
-                e.preventDefault();
-                return false;
-            }
+//    if(Modernizr.touch){
+        jQuery('#topnav li.menu-item > a').dblclick(function(e){
             jQuery(this).data('open', true);
+            jQuery(this).click();
         });
-    }
+        jQuery('#topnav li.menu-item > a').click(function(e){
+            children = jQuery(this).parent('li').children('ul');
+            if(children.length){
+                if(!jQuery(this).data('open')){
+                    e.preventDefault();
+                }
+            }
+        });
+//    }
 });
