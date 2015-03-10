2ec4ee379e5813865f2191547b0b83df7e3edf7d
Austin Gross <austing@gmail.com>
2015-03-10 17:38:54 +0100

    try script like this

--- scripts/androidDropDowns.js
+++ scripts/androidDropDowns.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function(){
     if(Modernizr.touch){
-        jQuery('#menu-mainmenu').on('click', '> li', function(e){
+        jQuery('#menu-mainmenu').on('click', 'li.menu-item > a', function(e){
             if(!jQuery(this).data('open')){
                 e.preventDefault();
+                return false;
             }
             jQuery(this).data('open', true);
         });
